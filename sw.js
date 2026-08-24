4364a2a478a74b134a63a98b51080b540de10048
sadehlin <sadehlin@gmail.com>
2026-08-24 16:44:44 -0700

    Add offline app shell

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C='sbm-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['/','/index.html','/manifest.json']))));self.addEventListener('fetch',e=>{if(e.request.method==='GET'&&new URL(e.request.url).origin===location.origin)e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))})
